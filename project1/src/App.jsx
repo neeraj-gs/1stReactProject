d9c17e19ec8fc72daf35ef817ea89dab5fa5d033
neeraj-gs <gsneeraj05@gmail.com>
2023-07-19 18:45:14 +0530

    Added a Header compoentn and added it to amin app.jsx file as well

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -1,9 +1,12 @@
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import Header from "./components/Header"
+
 
 function App() {
   return (
     <>
     <Router>
+      <Header />
       <Routes>
         <Route />
       </Routes>
