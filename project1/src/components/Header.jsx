beabf4506746daf54f98bb70cf9cc9e1b56bf424
neeraj-gs <gsneeraj05@gmail.com>
2023-07-20 10:50:34 +0530

    Added a Header Component that is always rendered ireespective of route

--- project1/src/components/Header.jsx
+++ project1/src/components/Header.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav>
+      <h1>TechBuzz...</h1>
+      <main>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/contact"}>Contact</Link>
+        <Link to={"/#about"}>About</Link>
+        <Link to={"/#blogs"}>Blogs</Link>
+        <Link to={"/services"}>Services</Link>
+      </main>
+    </nav>
   )
 }
 
